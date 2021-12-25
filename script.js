8e71f0b6466f11449c06ab45c9a2a18f55db782d
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2021-12-25 20:01:08 +0100

    better code structure and added the date of the day on the watch

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.addEventListener("readystatechange", (event) => {
   if (event.target.readyState === "complete") {
     clock();
+    requestAnimationFrame(animate);
+    textElement.addEventListener("click", () => {
+      showDate = !showDate;
+    });
   }
 });
 
@@ -8,34 +12,39 @@ const hourHand = document.getElementById("hour_hand");
 const minuteHand = document.getElementById("minute_hand");
 const secondHand = document.getElementById("second_hand");
 const timer = document.getElementById("timer");
+const textElement = document.getElementById("text");
+
+let showDate = true;
 
 const getTime = () => {
   const today = new Date();
-  let hours = today.getHours();
-  let minutes = today.getMinutes();
-  let seconds = today.getSeconds();
-  let milliseconds = today.getMilliseconds();
-  let time = {
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
+  const seconds = today.getSeconds();
+  const milliseconds = today.getMilliseconds();
+  const day = today.getDate();
+  const time = {
     hours: hours,
     minutes: minutes,
     seconds: seconds,
     milliseconds: milliseconds,
+    day: day,
   };
   return time;
 };
 
 const animate = () => {
-  let { hours, minutes, seconds, milliseconds } = getTime();
+  let { hours, minutes, seconds, milliseconds, day } = getTime();
+  const ampm = hours >= 12 ? "PM" : "AM";
   hours = hours + minutes / 60;
   minutes = minutes + seconds / 60;
   seconds = seconds + milliseconds / 1000;
+  textElement.textContent = showDate ? day : ampm;
   hourHand.setAttribute("transform", `rotate(${(360 / 12) * hours})`);
   minuteHand.setAttribute("transform", `rotate(${(360 / 60) * minutes})`);
   secondHand.setAttribute("transform", `rotate(${(360 / 60) * seconds})`);
   requestAnimationFrame(animate);
 };
-
-requestAnimationFrame(animate);
 
 const clock = () => {
   const { hours, minutes, seconds } = getTime();
