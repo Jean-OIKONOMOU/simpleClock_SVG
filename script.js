6afd4d4fc00214c48757b5f7606c467921fda26d
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2021-12-25 19:28:19 +0100

    the clock's hands now move

--- script.js
+++ script.js
@@ -4,15 +4,39 @@ document.addEventListener("readystatechange", (event) => {
   }
 });
 
+const hourHand = document.getElementById("hour_hand");
+const minuteHand = document.getElementById("minute_hand");
+const secondHand = document.getElementById("second_hand");
+const timer = document.getElementById("timer");
+
+const getTime = () => {
+  const today = new Date();
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
+  const seconds = today.getSeconds();
+  let time = {
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+  };
+  return time;
+};
+
+const animate = () => {
+  const { hours, minutes, seconds } = getTime();
+  hourHand.setAttribute("transform", `rotate(${(360 / 12) * hours})`);
+  minuteHand.setAttribute("transform", `rotate(${(360 / 60) * minutes})`);
+  secondHand.setAttribute("transform", `rotate(${(360 / 60) * seconds})`);
+  requestAnimationFrame(animate);
+};
+
+requestAnimationFrame(animate);
+
 const initApp = () => {
-  let timer = document.getElementById("timer");
-  let today = new Date();
-  let hours = today.getHours();
-  let minutes = today.getMinutes();
-  let seconds = today.getSeconds();
+  const { hours, minutes, seconds } = getTime();
   timeString = (hours < 10 ? "0" : " ") + hours;
-  timeString += (minutes < 10 ? " :0" : " : ") + minutes;
-  timeString += (seconds < 10 ? " :0" : " : ") + seconds;
+  timeString += (minutes < 10 ? " : 0" : " : ") + minutes;
+  timeString += (seconds < 10 ? " : 0" : " : ") + seconds;
   timer.innerText = timeString;
   setTimeout("initApp()", 1000);
 };
