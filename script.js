2ea0f4d860560d8edc798caa7a7f8e4fad3a87dc
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2021-12-25 18:52:34 +0100

    hours and minutes represented

--- script.js
+++ script.js
@@ -6,15 +6,13 @@ document.addEventListener("readystatechange", (event) => {
 
 const initApp = () => {
   let timer = document.getElementById("timer");
-  console.log(timer);
   let today = new Date();
   let hours = today.getHours();
   let minutes = today.getMinutes();
   let seconds = today.getSeconds();
-
   timeString = (hours < 10 ? "0" : " ") + hours;
-  timeString += (minutes < 10 ? ":0" : " : ") + minutes;
-  timeString += (seconds < 10 ? ":0" : " : ") + seconds;
+  timeString += (minutes < 10 ? " :0" : " : ") + minutes;
+  timeString += (seconds < 10 ? " :0" : " : ") + seconds;
   timer.innerText = timeString;
   setTimeout("initApp()", 1000);
 };
