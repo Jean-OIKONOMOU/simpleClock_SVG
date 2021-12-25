16f7e00c895064ac6b06ba376715c29adaf92b6b
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2021-12-25 18:26:59 +0100

    added readme, basic files, and a simple timer

---
+++ README.md
@@ -0,0 +1,7 @@
+# What is this ?
+
+First time I am actually creating SVG inside an HTML file. Before that I used to just import SVG images and have trouble using them because I did not really care how they worked. I have decided to actually create something in SVG, and manipulate it using JS, in order to familiarize myself with that technology.
+
+# What will you be doing ?
+
+Simple animated SVG clock.
---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    initApp();
+  }
+});
+
+const initApp = () => {
+  let timer = document.getElementById("timer");
+  console.log(timer);
+  let today = new Date();
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+  let seconds = today.getSeconds();
+
+  timeString = (hours < 10 ? "0" : " ") + hours;
+  timeString += (minutes < 10 ? ":0" : " : ") + minutes;
+  timeString += (seconds < 10 ? ":0" : " : ") + seconds;
+  timer.innerText = timeString;
+  setTimeout("initApp()", 1000);
+};
